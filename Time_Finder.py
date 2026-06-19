051aede48691a7bb50c7abd5b693846c9c31ed35
Krishna20122008 <kchourasia121@gmail.com>
2026-06-19 10:26:14 +0530

    Day-05: Pushed Time_Finder.py

---
+++ Time_Finder.py
@@ -0,0 +1,6 @@
+Distance=int(input("Enter the distance to be travelled: "))
+Speed=int(input("Enter average speed with which vehicle is travelling: "))
+Time=Distance/Speed
+print("Distance is ",Distance, " km")
+print(f"Speed is {Speed} kmph")
+print("Time taken: ",Time," hr")
