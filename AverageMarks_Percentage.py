24f0ad613d56bff0f0178f24310d5fd2acf82e47
Krishna20122008 <kchourasia121@gmail.com>
2026-06-20 21:08:25 +0530

    Day-06: Pushed AverageMarks_Percentage.py

---
+++ AverageMarks_Percentage.py
@@ -0,0 +1,7 @@
+maths=float(input("enter the value of maths:"))
+bio=float(input("enter the value of bio:"))
+physics=float(input("enter the value of physics:"))
+chem=float(input("enter the value of chem:")) 
+Eng=float(input("enter the value of Eng:")) 
+print("average of marks of five subjects is:", ((maths+bio+physics+chem+Eng)/5))
+print("percentage of child is:", (((maths+bio+physics+chem+Eng)/400)*100) ,"%")
