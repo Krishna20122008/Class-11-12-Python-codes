e6cae812ff3fdfb1aad62236bbe3abb701d56f9e
Krishna20122008 <kchourasia121@gmail.com>
2026-06-21 11:01:33 +0530

    DAy

---
+++ Income_tax.py
@@ -0,0 +1,24 @@
+a=(input("Are you a politician? y/n:"))
+
+if a=="y":
+    print("You don't have to pay any tax.")
+
+elif a=="n":
+    print("Then you are a salary receiver.")
+
+    i=int(input("Enter your Income:"))
+
+    if i<=50000:
+        print("The tax you have to pay is:₹",  (i*0.005))
+
+    elif i>50000 and i<=100000:
+        print("The tax you have to pay is:₹",(i*0.2))
+
+    elif i>=120000:
+        print("The tax you have to pay is:₹",(i*0.5))
+
+input()
+
+
+
+
