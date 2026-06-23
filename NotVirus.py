1e660211a13fc4e38daaa0810c6e524453c15642
Krishna20122008 <kchourasia121@gmail.com>
2026-06-23 22:47:12 +0530

    Day-09: Pushed NotVirus.py

---
+++ NotVirus.py
@@ -0,0 +1,19 @@
+print("Your pc has been hacked!")
+a="y"
+while a=="y":
+    a=input("Do you want to remove the virus? y/n: ")
+    if a=="y":
+        print("You entered 'no', Try again")
+    else:
+        print("You entered no")
+        print("Your order is out priority, Extracting the virus on your pc \n")
+        print("Your pc has been hacked!") 
+
+import time
+time.sleep(6)  
+x=2
+while x>1:
+    print("Hacked               Hacked              Hacked              Hacked              Hacked              Hacked")
+    
+
+input()
