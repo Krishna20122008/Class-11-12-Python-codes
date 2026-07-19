0d607d39e5b7a59384af638c8039f3e1dcd801de
Krishna20122008 <kchourasia121@gmail.com>
2026-07-19 12:36:47 +0530

    Pushed StarRectangle.py

---
+++ PatternPrinting/StarRectangle.py
@@ -0,0 +1,7 @@
+a = int(input ("Enter No. of rows: "))
+b = int(input ("Enter No. of columns: "))
+
+for i in range(1,a+1):
+    for j in range(1,b+1):
+        print("*", end = " ")
+    print()
--- Series sum.py
+++
@@ -1,8 +0,0 @@
-x=int(input("Enter the number:"))
-i=0
-s=0
-n=int(input("Enter the number till where to sum"))
-while i<=n:
-    s+=x**i
-    print(s)
-    i+=1
